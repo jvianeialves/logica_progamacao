5906a2554cf851b91d6355e2708b594b24d637b9
jvianeialves <Jvianney@live.com>
2025-06-15 18:23:05 -0300

    Add files via upload

---
+++ logica de progamacao/unidade_2.py
@@ -0,0 +1,52 @@
+print('hello wold!' )
+#questao_01
+nome = "José Vianney Alves"
+idade = 83
+print(f"Meu nome é {nome}, eu tenho {idade} anos.")
+#
+#questão_02
+#
+A = 5
+B = 10
+soma=A+B
+print(f"O valor das variaveis A e B somadas será {soma},se o valor for invertido tambem será {soma}")
+#
+# Questão_3, Definindo o valor da area de um circulo. utilizado como material de apoio a "LUZIA" do WATSapp,(https://luzia.onelink.me/e1qV/wbr)
+pi = 3.14159
+raio = 4
+area = pi * (raio ** 2)
+print("A área do círculo com raio", raio, "é:",area)
+#
+#questão_4 criando as tres variaveis
+valor = 10
+peso = 3.14
+nome = "Zé da manga"
+#fazendo o uso do comando "TYPE" conteudo do video_01 
+print(f'A variavel "valor" é do tipo {type(valor)}.')
+print(f'A variavel "peso" é do tipo {type(peso)}.')
+print(f'A variavel "nome" é do tipo {type(nome)}.')
+#
+#questão_5 criando a espressão
+ESPRESSAO = 10+5*2-3**2
+print(ESPRESSAO)
+#
+#questão_6 criando a variavel
+def celsius_para_fahrenheit(celsius):
+   fahrenheit = (celsius * 9/5) + 32
+   return fahrenheit
+graus_celsius = 25
+graus_fahrenheit = celsius_para_fahrenheit(graus_celsius)
+print(f"{graus_celsius} graus Celsius é equivalente a {graus_fahrenheit} graus Fahrenheit.")
+#
+#questão_7 criando a verificador de variaveis
+#
+#questão_8 avaliando a expressao (5 > 3) and (2 < 1)
+print(f"A operação (5 > 3) and (2 < 1)trata se de uma operação booleana, onde o retorno deve atender que ambos os parentesis devem ser verddadeiro para ser TRUE, assim sendo ela sera falsa pois somente o o resultado 5>3 é verdadeiro.")
+#
+#questão_9 criando
+altura_str = "1.75"
+altura_str = float(altura_str)
+altura_float = altura_str
+print(f"A altura convertida é: {altura_float}")
+#
+#questão_10 criando conjuntos
